074d564de2c6738f5ca147b374697acc7c4f9638
talentotech-ba <talento.tech@bue.edu.ar>
2024-10-10 12:06:35 -0300

    Update test_mostrar.py

--- tests/test_mostrar.py
+++ tests/test_mostrar.py
@@ -1,15 +1,16 @@
-
+import unittest
 import app  # Asegúrate de que este sea el nombre del archivo que contiene tu código
 from unittest.mock import patch
-# Test para mostrar productos cuando no hay productos
-@patch('builtins.input', side_effect=["2", "3"])  # Simulamos la opción de mostrar productos
-def test_mostrar_productos_sin_productos(mock_input):
-    app.productos = []  # Aseguramos que la lista esté vacía
-    app.main()  # Ejecutamos el programa
-    assert len(app.productos) == 0  # Verificamos que la lista sigue vacía
 
-# Test para la opción de salir
-@patch('builtins.input', side_effect=["3"])  # Simulamos la opción de salida
-def test_salir(mock_input):
-    app.main()  # Ejecutamos el programa
-    assert True  # Verificamos que el programa termina sin errores
+# Test para mostrar productos cuando no hay productos
+class TestApp(unittest.TestCase):
+        
+    @patch('builtins.input', side_effect=["2", "3"])  # Simulamos la opción de mostrar productos
+    def test_mostrar_productos_sin_productos(self, mock_input):
+        app.main()  # Ejecutamos el programa
+        
+        with self.subTest("Verificando cantidad de productos"):
+            self.assertEqual(len(app.productos), 0, "Por defecto no hay productos" )
+
+if __name__ == '__main__':
+    unittest.main()
